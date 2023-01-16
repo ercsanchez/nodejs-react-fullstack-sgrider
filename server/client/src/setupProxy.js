c1a8787c9404deeeb232ac098e28b580ba0c15c8
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-01-16 18:48:32 +0800

    7.20 adjust proxy so that it matches all url patterns for the api route (/api/any-pattern)

--- server/client/src/setupProxy.js
+++ server/client/src/setupProxy.js
@@ -2,7 +2,8 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    ['/api', '/auth/google'],
+    // /api/* ensures that route /api or /api/  won't match but nested routes (e.g. /api/any-char) will match
+    ['/api/*', '/auth/google'],
     createProxyMiddleware({
       target: 'http://localhost:5000',
     })
