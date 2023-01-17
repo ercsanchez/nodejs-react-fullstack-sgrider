f59073f529011667be3c88962beb731ebc1bbdbf
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-01-17 14:01:32 +0800

    7.24-7.25 use async/await to simplify action | attach only response data in payload

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -2,19 +2,16 @@ import axios from 'axios';
 
 import { FETCH_USER } from './types';
 
-export const fetchUser = () => {
-  // usual action if using vanilla redux
-  // const request = await axios.get('/api/current_user', async (req, res) => {
-  //   return {
-  //     type: FETCH_USER,
-  //     payload: request,
-  //   };
-  // });
-
-  // if using redux-thunk
-  return function (dispatch) {
-    return axios
-      .get('/api/current_user')
-      .then((res) => dispatch({ type: FETCH_USER, payload: res }));
-  };
+export const fetchUser = () => async (dispatch) => {
+  // action if using redux-thunk
+  const res = await axios.get('/api/current_user');
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
+
+// usual action if using vanilla redux
+// const request = await axios.get('/api/current_user', async (req, res) => {
+//   return {
+//     type: FETCH_USER,
+//     payload: request,
+//   };
+// });
