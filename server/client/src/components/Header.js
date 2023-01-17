cc297f12d050d202c4dab1177f64e4bef0b97e7d
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-01-17 18:30:14 +0800

    7.31 use Link react comp for conditional rendering when user clicks App logo on header

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   renderContent() {
@@ -25,9 +26,13 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a href="/" className="brand-logo left">
+          <Link
+            to={this.props.auth ? '/surveys' : '/'}
+            href="/"
+            className="brand-logo left"
+          >
             App
-          </a>
+          </Link>
           <ul id="nav-mobile" className="right">
             {/* <li>
               <a href="/">Login with Google</a>
