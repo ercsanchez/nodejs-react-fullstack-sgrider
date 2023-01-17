122fe93967e4cf8223e5b5ae9b50f21fce899deb
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-01-17 15:12:16 +0800

    7.27 finalize Header comp

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -5,11 +5,19 @@ class Header extends Component {
   renderContent() {
     switch (this.props.auth) {
       case null: // pending auth request
-        return 'still deciding';
+        return;
       case false: // not logged in
-        return 'im logged out';
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
       default: // logged in | this.props.auth contains an object
-        return 'im logged in';
+        return (
+          <li>
+            <a href="/">Logout</a>
+          </li>
+        );
     }
   }
   render() {
