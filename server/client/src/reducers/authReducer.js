f0add89630d0deabbe11da7e877939534943973d
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-01-17 14:24:25 +0800

    7.25 get authReducer to handle cases for user is logged in, not logged in, and waiting on api call for auth | convert empty response.data into false value

--- server/client/src/reducers/authReducer.js
+++ server/client/src/reducers/authReducer.js
@@ -1,8 +1,13 @@
-const authReducer = (state = {}, action) => {
-  console.log(action);
+import { FETCH_USER } from '../actions/types';
+
+const authReducer = (state = null, action) => {
+  // default state is null if api request (action creator) is still in process or something went wrong during auth
+  // console.log(action);
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false; // convert empty strings (empty response.data of api call) into false value
     default:
-      return state || null;
+      return state;
   }
 };
 
